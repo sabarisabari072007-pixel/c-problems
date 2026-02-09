619022a9f5d82002d0212a0a01a74ad4c81b8e33
sabarisabari072007-pixel <sabarisabari072007@gmail.com>
2026-02-09 11:44:06 +0530

    Add files via upload

---
+++ p6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char a[]="Age";
+    int b=20;
+    char c[]="Count";
+    int d=5;
+    printf("%s:""%d\n",a,b);
+    printf("%s:""%d",c,d);
+    return 0;
+}
