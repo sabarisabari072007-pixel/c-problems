f6c779e0f47039ac8acba56e92a4cd136e6f5294
sabarisabari072007-pixel <sabarisabari072007@gmail.com>
2026-02-10 12:17:23 +0530

    Update p53.c

--- p53.c
+++ p53.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int a,b;
+    int a,b,c;
     scanf("%d" "%d",&a,&b);
-    printf("%d ",a/b);
-    printf("%d",a%b);
+    c=a;
+    printf("%d ",a/=b);
+    printf("%d",c%=b);
     return 0;
 }
