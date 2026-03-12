8e09acce8ae310eb857d64511d4207e6ac455d96
sabariromeo <sabarisabari072007@gmail.com>
2026-03-12 10:11:27 +0530

    Create str12.c

---
+++ str12.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char s[50];int count=0;
+    scanf("%s",s);
+    for(int i=0;i<strlen(s);i++){
+        if(s[i]=='@' || s[i]=='#' || s[i]=='$' || s[i]=='%' || s[i]=='&'){
+        count++;
+        }
+    }
+    printf("%d",count);
+    return 0;
+}
