710039333eca891f4f8f01eb1516da59773a8ac6
sabariromeo <sabarisabari072007@gmail.com>
2026-03-16 09:50:37 +0530

    Add files via upload

---
+++ fun8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int revnum(int n){
+int rev=0;
+while(n!=0){
+    rev=rev*10+n%10;
+    n=n/10;
+}
+return rev;
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    printf("%d",revnum(n));
+    return 0;
+}
